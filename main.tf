a5e32be74c126d512c386b85df784a43d1388c30
Joe Sak <joe@joesak.com>
2020-05-04 00:00:09 -0500

    Change ubuntu -> ec2-user ?

--- main.tf
+++ main.tf
@@ -107,7 +107,7 @@ resource "aws_instance" "web" {
   # communicate with the resource (instance)
   connection {
     # The default username for our AMI
-    user = "ubuntu"
+    user = "ec2-user"
     host = self.public_ip
     # The connection will use the local SSH agent for authentication.
   }
